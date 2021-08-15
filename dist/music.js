03ae9b9ae4ff4c766ab87d1f2d37bddb161a1b0d
luozj1020 <2533391675@qq.com>
2021-08-15 10:27:21 +0800

    Site updated: 2021-08-15 10:27:19

---
+++ dist/music.js
@@ -0,0 +1,43 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  audio: [
+{
+      name: "エンドレスワルツ (赤城・加賀戦)",
+      artist: '東京アクティブNEETs',
+      url: 'http://music.163.com/song/media/outer/url?id=574908784.mp3',
+      cover: 'http://p1.music.126.net/rlcmuLftmDgRhdfPDR39eQ==/109951163373949947.jpg?param=130y130',
+    },
+    {
+      name: "僕らのあしあと",
+      artist: 'supercell',
+      url: 'http://music.163.com/song/media/outer/url?id=825322.mp3',
+      cover: 'http://p1.music.126.net/sLzJCxw7pe9DErFqbbmr2Q==/109951166200260803.jpg?param=130y130',
+    },
+    {
+      name: "不可解",
+      artist: '花譜',
+      url: 'http://music.163.com/song/media/outer/url?id=1399849876.mp3',
+      cover: 'http://p2.music.126.net/oO42TdpWe0WG-rGq41mVIQ==/109951164456169034.jpg?param=130y130',
+    },
+    {
+      name: "Go→Love & Peace",
+      artist: '彩音',
+      url: 'http://music.163.com/song/media/outer/url?id=29027272.mp3',
+      cover: 'http://p1.music.126.net/zLg_Aq8aDxkqpZBCcEqQfg==/6620159511042599.jpg?param=130y130',
+    },
+    {
+      name: "如常",
+      artist: 'HITA',
+      url: 'http://music.163.com/song/media/outer/url?id=1393444357.mp3',
+      cover: 'http://p2.music.126.net/zS5UUwLqg7w-NgVYed5xMg==/109951164388141414.jpg?param=130y130',
+    },
+    {
+      name: "オルフェンズの涙",
+      artist: 'MISIA',
+      url: 'http://music.163.com/song/media/outer/url?id=37095620.mp3',
+      cover: 'http://p1.music.126.net/i6RomK-9BWVck0Pk6y2CmQ==/109951165049890849.jpg?param=130y130',
+    },
+  ]
+});
